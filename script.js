d2b706809481753f003e523aed3c22cc22f8e70a
SEANYB4 <bainsean@outlook.com>
2023-06-12 08:05:24 +0300

    Added basic enemy

--- script.js
+++ script.js
@@ -25,6 +25,137 @@ window.addEventListener('load', () => {
     dialogueText.innerHTML = 'This is edited by JS....';
 
 
+    class Enemy {
+
+        constructor(game, x, y) {
+
+            this.game = game;
+
+            // Enemy stats
+
+            this.attackStrength = 10;
+            this.attacking = false;
+
+
+
+            this.images = {
+                idle: './EnemyIdle.png',
+                idle2: './EnemyIdle2.png',
+                jump: './EnemyJump.png',
+                run: './EnemyRun.png',
+                run2: './EnemyRun2.png',
+                attack: './EnemyAttack.png',
+
+            }
+
+            this.image = new Image();
+            this.image.src = './EnemyIdle2.png';
+            
+
+            this.x = x;
+            this.y = y;
+            this.width = 70;
+            this.height = 71;
+            this.yVelocity = 5
+            this.xVelocity = 0;
+            this.spriteX = 0;
+            this.spriteY = 0;
+            
+
+        }
+
+        draw(context) {
+
+            // Enemy Animation
+
+                if (animationCount == 0) {
+                    this.spriteX = this.spriteX+95.8
+                    if (this.spriteX >= 514) {
+                        this.spriteX = 0;
+                        if (this.attacking){
+                            this.attacking = false;
+                        }
+                    }
+                }
+
+
+            context.drawImage(this.image, this.spriteX, this.spriteY, this.width, this.height, this.x, this.y, this.width, this.height);
+        
+            
+        }
+
+        update() {
+
+
+            // ENEMY AI
+            if (this.attacking) {
+                this.image.src = this.images.attack;
+            }
+
+            else if (this.x >= this.game.player.x + 40) {
+                this.xVelocity = -5;
+                this.image.src = this.images.run2;
+            } else if (this.x <= this.game.player.x - 40) {
+                this.xVelocity = 5;
+                this.image.src = this.images.run;
+            } else if (((this.x <= (this.game.player.x + 40)) || (this.x >= (this.game.player.x - 40)) && 
+                        (this.y <= (this.game.player.y + 5)) || (this.y >= (this.game.player.y - 5))
+                )) {
+                this.xVelocity = 0;
+                
+                if((Math.random()*50)>49){
+                    
+                    this.attacking = true;
+                    this.attack();
+                } else {
+                    this.image.src = this.images.idle;
+                }
+                
+
+            } else if (this.x <= this.game.player.x + 40 || this.x >= this.game.player.x -40) {
+                this.image.src = this.images.idle;
+            } else if (this.game.dead) {
+                this.image.src = this.images.idle;
+            }
+
+
+            
+
+            for (let i = 0; i < this.game.platformPositions.length; i++) {
+
+                // vertical collision detection
+
+                if (((this.x+(this.width)-40) >= this.game.platformPositions[i][0] && (this.x <= this.game.platformPositions[i][0]+(this.game.platforms[0].width-20))) &&
+                    (this.y+this.height) <= (this.game.platformPositions[i][1]+1) && (this.y+this.height) >= this.game.platformPositions[i][1]-.1) {
+                        this.yVelocity = 0;
+                        break
+
+                } else {
+                    this.yVelocity = 5;
+                }
+
+                
+            }
+ 
+    
+            this.y += this.yVelocity;
+            this.x += this.xVelocity;
+
+            
+            this.xVelocity = 0;
+            
+        }
+
+
+        attack() {
+
+            this.game.player.health -= this.attackStrength;
+
+
+
+        }
+    }
+
 
 
 
@@ -49,6 +180,8 @@ window.addEventListener('load', () => {
 
         draw(context) {
 
+
+
             context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.floatY, this.width, this.height);
             if (this.down){
                 this.floatY += .05
@@ -68,15 +201,31 @@ window.addEventListener('load', () => {
         }
     }
 
+
+    class JumpEffect {
+
+        constructor(game) {
+
+            this.game = game;
+            this.image = new Image();
+            this.image.src = './jump-effect.png';
+
+        }
+    }
+
     class Player {
 
         constructor(game) {
             this.game = game;
 
+            this.health = 100;
 
             this.images = {
                 idle: './Idle.png',
-                jump: './Jump.png'
+                jump: './Jump.png',
+                run: './Run.png',
+                run2: './Run2.png',
+                attack: './Attack_1.png',
 
             }
 
@@ -86,7 +235,7 @@ window.addEventListener('load', () => {
 
             this.x = 50;
             this.y = 50;
-            this.width = 34;
+            this.width = 90;
             this.height = 71;
             this.yVelocity = 5
             this.xVelocity = 0;
@@ -98,15 +247,32 @@ window.addEventListener('load', () => {
         draw(context) {
 
 
+            // Check which sprites to draw
+
+            // if (this.yVelocity !== 0){
+            //     this.image.src = this.images.jump;
+            // } else if(this.yVelocity == 0){
+            //     this.image.src = this.images.idle;
+            // }
+
+            // Player Animation
+
+            
+
+                if (animationCount == 0) {
+                    this.spriteX = this.spriteX+95.8
+                if (this.spriteX >= 610) {
+                    this.spriteX = 0;
+                }
+                }
+
+          
+
             context.drawImage(this.image, this.spriteX, this.spriteY, this.width, this.height, this.x, this.y, this.width, this.height);
             
 
-            if (animationCount == 0) {
-                this.spriteX = this.spriteX+95.8
-            if (this.spriteX >= 610) {
-                this.spriteX = 0;
-            }
-            }
+            
+
             
         }
 
@@ -118,7 +284,7 @@ window.addEventListener('load', () => {
 
                 // vertical collision detection
 
-                if (((this.x+this.width) >= this.game.platformPositions[i][0] && (this.x <= this.game.platformPositions[i][0]+(this.game.platforms[0].width))) &&
+                if (((this.x+(this.width)-40) >= this.game.platformPositions[i][0] && (this.x <= this.game.platformPositions[i][0]+(this.game.platforms[0].width-20))) &&
                     (this.y+this.height) <= (this.game.platformPositions[i][1]+1) && (this.y+this.height) >= this.game.platformPositions[i][1]-.1) {
                         this.yVelocity = 0;
                         break
@@ -136,7 +302,7 @@ window.addEventListener('load', () => {
 
             
             this.xVelocity = 0;
-            this.yVelocity = 0;
+            
         }
     }
 
@@ -156,11 +322,41 @@ window.addEventListener('load', () => {
             this.backgroundImage.onload = () => {
                 ctx.drawImage(this.backgroundImage, 0, 0, this.width, this.height);
             } 
+
+
+            // create platforms
             this.platforms = [];
-            this.platformPositions = [[50, 150], [140, 110], [500, 150], [500, 150], [300, 350,], [0, this.height-50], [(this.width/5)*2, this.height-50], [(this.width/5)*4, this.height-50], [(this.width/5)*3, this.height-200]];
+            this.platformPositions = [[50, 150], 
+                                    [140, 110], 
+                                    [500, 150], 
+                                    [500, 150], 
+                                    [300, 350,], 
+                                    [0, this.height-50], 
+                                    [50, this.height-50], 
+                                    [100, this.height-50], 
+                                    [150, this.height-50], 
+                                    [200, this.height-50],
+                                    [250, this.height-50], 
+                                    [300, this.height-50], 
+                                    [350, this.height-50], 
+                                    [400, this.height-50], 
+                                    [450, this.height-50],  
+                                    [(this.width/5)*2, this.height-50], 
+                                    [(this.width/5)*4, this.height-50], 
+                                    [(this.width/5)*3, this.height-200]];
 
             for(let i = 0; i < this.platformPositions.length; i++) {
                 this.platforms.push(new Platform(this, this.platformPositions[i][0], this.platformPositions[i][1]));
+            }
+
+
+            // create enemies
+
+            this.enemies = [];
+            this.enemyPositions = [[500, 50]];
+
+            for(let i = 0; i < this.enemyPositions.length; i++) {
+                this.enemies.push(new Enemy(this, this.enemyPositions[i][0], this.enemyPositions[i][1]));
             }
             
             this.dead = false;
@@ -182,19 +378,20 @@ window.addEventListener('load', () => {
 
                 if (event.key === 'a') {
                     this.player.xVelocity = -20;
+                    this.player.image.src = this.player.images.run2;
                     
                 } else if (event.key === "d") {
                     this.player.xVelocity = 20;
-                    for (let i = 0; i < this.platformPositions.length; i++) {
-
-                        
-                    }
+                    this.player.image.src = this.player.images.run;
                 } else if (event.key === 'w') {
-                    
+                    this.player.image.src = this.player.images.jump;
                     this.player.y -= 80;
                     
                 } else if (event.key === 's') {
                     this.player.yVelocity = 10
+                } else if(event.key === ' ') {
+                    this.player.animationCount = 0;
+                    this.player.image.src = this.player.images.attack;
                 }
 
 
@@ -203,11 +400,19 @@ window.addEventListener('load', () => {
             })
             document.addEventListener('keyup', (event) => {
 
-                if (event.code === 'ArrowLeft') {
+                if (event.key === 'a') {
                     this.player.xVelocity = 0;
-                } else if (event.code === "ArrowRight") {
+                    this.player.image.src = this.player.images.idle;
+                } else if (event.key === "d") {
                     this.player.xVelocity = 0;
-                } 
+                    this.player.image.src = this.player.images.idle;
+                } else if (event.key === 'w') {
+                    this.player.image.src = this.player.images.idle;
+                    this.player.yVelocity = 0;
+                } else if (event.key === ' ') {
+                    this.player.image.src = this.player.images.idle;
+                    
+                }
 
             })
 
@@ -222,7 +427,9 @@ window.addEventListener('load', () => {
 
             this.player.x = 50;
             this.player.y = 50;
+            
             this.dead = false;
+            music.play();
 
 
         }
@@ -242,9 +449,16 @@ window.addEventListener('load', () => {
 
                 this.platforms[i].draw(context);
                 
-
-
             }
+
+            // draw enemies on the screen
+
+            this.enemies.forEach((i) => {
+
+                i.draw(context);
+                i.update();
+
+            });
 
             // draw the player on the canvas
             this.player.draw(context);
